2df8ffa23e04d4877f9ef7064a29501cde62cb5e
Janu shankar <janakiharishankar24@gmail.com>
2022-01-28 22:34:14 +0530

    Exception

--- CustomException.cs
+++ CustomException.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace Moodanalyser
 {
-    internal class MoodAnalysisException
+
+    internal class CustomException
     {
+        string message;
+        public CustomException(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (message.Equals(string.Empty))
+                {
+                    throw (new CustomException(CustomException.ExceptionType.EMPTY_EXCEPTION, "Mood should not be empty"));
+                }
+
+                else if (message.ToLower().Contains("happy"))
+                {
+                    return "happy";
+                }
+                else
+                {
+                    return "sad";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return "happy";
+
+            }
+        }
     }
 }
+
+    
+
+
+
