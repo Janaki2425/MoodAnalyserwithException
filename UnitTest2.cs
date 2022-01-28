57f2d4476e2474d3cc5acaba7f6541f3ee2edd9b
Janu shankar <janakiharishankar24@gmail.com>
2022-01-28 22:21:29 +0530

    test case 2

---
+++ UnitTest2.cs
@@ -0,0 +1,105 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moodanalyser;
+    
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+       
+          [TestMethod]
+            public void reflectionDefaultConstructor()
+            {
+                MoodAnalyser expected = new MoodAnalyser();
+                object obj = null;
+                try
+                {
+                    MoodAnalyserFactory factory = new MoodAnalyserFactory();
+                    obj = factory.CreateMoodAnalyserObject("MoodAnalyserProblem2.MoodAnalyser", "MoodAnalyser");
+
+                }
+                catch (CustomException ex)
+                {
+                    throw new System.Exception(ex.Message);
+                }
+            }
+            
+            [TestMethod]
+            public void defaultConstructorNoClassFound()
+            {
+                string expected = "Class not found";
+                object obj = null;
+                try
+                {
+                    MoodAnalyserFactory factory = new MoodAnalyserFactory();
+                    obj = factory.CreateMoodAnalyserObject("MoodAnalyserProblem2.MoodAnaly", "MoodAnaly");
+
+                }
+                catch (CustomException actual)
+
+                {
+                    Assert.AreEqual(expected, actual.Message);
+                }
+            }
+            [TestMethod]
+            public void defaultConstructorNoConstructorFound()
+            {
+                string expected = "Constructor not found";
+                object obj = null;
+                try
+                {
+                    MoodAnalyserFactory factory = new MoodAnalyserFactory();
+                    obj = factory.CreateMoodAnalyserObject("MoodAnalyserProblem2.MoodAnalyser", "MoodAnaly");
+
+                }
+                catch (CustomException actual)
+                {
+                    Assert.AreEqual(expected, actual.Message);
+                }
+            }
+
+            
+            [TestMethod]
+            public void ReflectionParameterizedConstructor()
+            {
+                string message = "I am in happy mood";
+                MoodAnalyser expected = new MoodAnalyser(message);
+                object actual = null;
+                try
+                {
+                    MoodAnalyserFactory factory = new MoodAnalyserFactory();
+                    actual = factory.CreateMoodAnalyserParameterizedObject("MoodAnalyserProblem.MoodAnalyser", "MoodAnalyser", message);
+
+                }
+                catch (CustomException ex)
+                {
+                    throw new System.Exception(ex.Message);
+                }
+                actual.Equals(expected);
+            }
+           
+            [TestMethod]
+            public void returnParameterizedInvalidClass()
+            {
+                string message = "I am in happy mood";
+                MoodAnalyser expected = new MoodAnalyser(message);
+                object actual = null;
+                try
+                {
+                    MoodAnalyserFactory factory = new MoodAnalyserFactory();
+                    actual = factory.CreateMoodAnalyserParameterizedObject("MoodAnalyserProblem.MoodAna", "MoodAnalyser", message);
+
+                }
+                catch (CustomException actual1)
+                {
+                    Assert.AreEqual(expected, actual1.Message);
+                }
+            }
+
+        }
+    }
+
+
+    }
+}
